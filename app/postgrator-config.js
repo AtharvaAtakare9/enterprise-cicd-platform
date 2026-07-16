4d6231d7324ab56abfdbe938ac2db873f311c97f
AtharvaAtakare9 <atharvaatakare09@gmail.com>
2026-07-16 09:08:10 +0530

    Fix postgrator config

--- app/postgrator-config.js
+++ app/postgrator-config.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const path = require("path");
 
+const connectionString =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
 module.exports = {
   migrationPattern: path.join(__dirname, "migrations", "*.sql"),
   driver: "pg",
-  connectionString:
-    process.env.NODE_ENV === "test"
-      ? process.env.TEST_DATABASE_URL
-      : process.env.DATABASE_URL,
+  connectionString,
+  password: "postgres"
 };
